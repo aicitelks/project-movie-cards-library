4ee624180536bcc602b241a865199e7fed98ea79
Letícia Castro <aicitelks@gmail.com>
2021-07-22 17:39:58 -0300

    Requisito 6: ok. Componente <MovieCard /> implementado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,11 +4,25 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    const { subtitle, storyline, rating, imagePath } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = this.props;
 
     return(
       <div className="movie-card">
-        card
+
+        <div className="movie-card-body">
+
+          <h1 className="movie-card-title">{ title }</h1>
+          <h2 className="movie-card-subtitle">{ subtitle }</h2>
+          <h4 className="movie-card-storyline">{ storyline }</h4>
+          
+          <div className="movie-card-rating">
+            <div className="rating">
+              { rating }
+            </div>
+          </div>
+
+        </div>
+
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -14,6 +14,7 @@ class MovieList extends React.Component {
         { movies.map(({ title, subtitle, storyline, rating, imagePath }) => {
           return (
             <MovieCard key={ title }
+              title={ title }
               subtitle={ subtitle }
               storyline={ storyline }
               rating={ rating }
