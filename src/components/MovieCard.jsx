4f407ef714900c7bf1716f23ed38a4026c5c85f7
Letícia Castro <aicitelks@gmail.com>
2021-07-22 18:19:43 -0300

    Requisito 7: ok. Imagem renderizada

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,16 +4,17 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props;
+    const { title, subtitle, storyline, rating, imagePath} = this.props;
 
     return(
       <div className="movie-card">
 
-        <div className="movie-card-body">
+        <div className="movie-card-body">          
+          <img className="movie-card-image" src={ imagePath } alt=""/>          
 
-          <h1 className="movie-card-title">{ title }</h1>
-          <h2 className="movie-card-subtitle">{ subtitle }</h2>
-          <h4 className="movie-card-storyline">{ storyline }</h4>
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
           
           <div className="movie-card-rating">
             <div className="rating">
