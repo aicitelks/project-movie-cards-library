0bbed9217c288ccf8a4e2ad573429a18d4bf7d97
Letícia Castro <aicitelks@gmail.com>
2021-07-24 19:00:48 -0300

    Requisitos 12, 13, 14, 15 e 16 implementados durante o desenvolvimento

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,16 +10,16 @@ class MovieCard extends React.Component {
     return(
       <div className="movie-card">
 
-        <div className="movie-card-body">          
+        <div className="movie-card-body">
           <img className="movie-card-image" src={ imagePath } alt=""/>
 
           <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
-          <p className="movie-card-storyline">{ storyline }</p>               
+          <p className="movie-card-storyline">{ storyline }</p>
 
         </div>
 
-        <Rating rating={ rating }/> 
+        <Rating rating={ rating }/>
 
       </div>
     );
