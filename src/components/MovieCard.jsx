d0e90c5599ff54212d9ec97da4462a7f8a96a6ac
Letícia Castro <aicitelks@gmail.com>
2021-07-24 19:29:23 -0300

    corrigido erro linter

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,19 @@
 // implement MovieCard component here
 //
 import React from 'react';
-import Rating from './Rating';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props;
 
     return(
+
       <div className="movie-card">
 
         <div className="movie-card-body">
-          <img className="movie-card-image" src={ imagePath } alt=""/>
+          <img className="movie-card-image" src={ imagePath } alt="" />
 
           <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
@@ -20,7 +21,7 @@ class MovieCard extends React.Component {
 
         </div>
 
-        <Rating rating={ rating }/>
+        <Rating rating={ rating } />
 
       </div>
     );
