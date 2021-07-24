90a10fdeadaa40f4f011adf083af6bce7aa32db5
Letícia Castro <aicitelks@gmail.com>
2021-07-24 19:14:47 -0300

    Requisito 17 - implementado PropTypes em <Rating />

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,6 +1,7 @@
 // implement Rating component here
 //
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
@@ -15,5 +16,9 @@ class Rating extends React.Component {
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
