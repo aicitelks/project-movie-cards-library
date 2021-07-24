bcc10dff0ffc861b6418da05c28a9a27e93e345d
Letícia Castro <aicitelks@gmail.com>
2021-07-24 18:27:49 -0300

    componente <Rating /> implementado

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,8 +4,14 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
+
     return (
-      <div>Rating</div>
+      <div className="movie-card-rating">
+        <div className="rating">
+          { rating }
+        </div>
+      </div>
     );
   }
 }
