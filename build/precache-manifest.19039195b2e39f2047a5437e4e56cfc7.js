54c6597ce643c027d87a7d2eaf09984fc8b65b15
Letícia Castro <aicitelks@gmail.com>
2022-03-02 19:16:23 -0300

    Realizado o bild da aplicação

---
+++ build/precache-manifest.19039195b2e39f2047a5437e4e56cfc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9129324fa2218ea2b6c8aee084c52d6b",
+    "url": "/project-movie-cards-library/index.html"
+  },
+  {
+    "revision": "30be3bd5beeeab6f1141",
+    "url": "/project-movie-cards-library/static/css/main.e2f7de8e.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/project-movie-cards-library/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "30be3bd5beeeab6f1141",
+    "url": "/project-movie-cards-library/static/js/main.10dc7f45.chunk.js"
+  },
+  {
+    "revision": "b5cd559347f125a80e22",
+    "url": "/project-movie-cards-library/static/js/runtime-main.136c6182.js"
+  }
+]);
